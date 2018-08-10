82c1664e63a012e2ab0ae88ec17ac5d701f02881
Rintaro Okamura <rintaro.okamura@gmail.com>
2018-08-10 13:45:53 +0900

    Add permissive_hold

--- config.h
+++ config.h
@@ -43,6 +43,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
 
+#define PERMISSIVE_HOLD
+
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 27
